3dad5c7bada0795b71faa57434a4813cfee75052
ldmpub <ldmpub@gmail.com>
2014-09-02 19:18:07 +0000

    2 new strings translated in french

---
+++ src/menion/android/whereyougo/utils/ExceptionHandler.java
@@ -0,0 +1,16 @@
+package menion.android.whereyougo.utils;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+public class ExceptionHandler implements UncaughtExceptionHandler {
+
+  private static UncaughtExceptionHandler defaultHandler = Thread
+      .getDefaultUncaughtExceptionHandler();
+
+  @Override
+  public void uncaughtException(Thread thread, Throwable ex) {
+    LogWriter.log("error.log", ex);
+    defaultHandler.uncaughtException(thread, ex);
+  }
+
+}
---
+++ src/menion/android/whereyougo/utils/LogWriter.java
@@ -0,0 +1,35 @@
+package menion.android.whereyougo.utils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class LogWriter {
+  static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+  public static void log(String fname, String msg) {
+    String loc = menion.android.whereyougo.utils.FileSystem.getRoot() + File.separator + fname;
+    try {
+      FileWriter fstream = new FileWriter(loc, true);
+      PrintWriter out = new PrintWriter(fstream);
+      out.println("" + dateFormat.format(new java.util.Date()) + " - " + msg);
+      out.close();
+    } catch (IOException e) {
+    }
+  }
+
+  public static void log(String fname, Throwable ex) {
+    String loc = menion.android.whereyougo.utils.FileSystem.getRoot() + File.separator + fname;
+    try {
+      FileWriter fstream = new FileWriter(loc, true);
+      PrintWriter out = new PrintWriter(fstream);
+      out.println("" + dateFormat.format(new java.util.Date()) + ":");
+      ex.printStackTrace(out);
+      out.close();
+    } catch (IOException e) {
+    }
+  }
+}
